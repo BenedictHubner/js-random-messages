a792f53ebcc1c08f87830f34b282e189b81f6ef8
Ben Hubner <117365892+BenedictHubner@users.noreply.github.com>
2023-01-13 00:54:26 +0000

    Added months

--- random-messages.js
+++ random-messages.js
@@ -1,1 +1,8 @@
 /* Main script */
+
+/* Message component storage */
+
+const months = ['January', 'March', 'May', 'July', 'August', 'October', 'December',
+                'April', 'June', 'September', 'November',
+                'February'];
+
